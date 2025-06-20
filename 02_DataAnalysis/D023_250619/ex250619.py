85bea90e581ec3a3549e85d592b159caaf90cf10
htkim <gusxo909@gmail.com>
2025-06-20 14:54:33 +0900

    1

--- 02_DataAnalysis/D023_250619/ex250619.py
+++ 02_DataAnalysis/D023_250619/ex250619.py
@@ -1,5 +1,5 @@
 # python gui
-import tkinter
+# import tkinter
 # ERP(Enterprise Resource Planning): 기업의 업무 프로세스를 통합적으로 관리, 지원하는 소프트웨어
 # DashBoard GUI
 
@@ -176,7 +176,7 @@ import tkinter
 
 
 
-# radio button
+# 라디오 버튼
 # import tkinter as tk
 #
 # window = tk.Tk()
@@ -216,3 +216,198 @@ import tkinter
 #
 # # 항상 마지막에
 # window.mainloop()
+
+
+
+# pack 정적 배치(상대위치 배치)
+# import tkinter as tk
+#
+# window = tk.Tk()
+#
+# window.geometry("300x200")
+# window.title("Window Title")
+#
+# b1 = tk.Button(window, text='top')
+# b1_1 = tk.Button(window, text='top-1')
+# b2 = tk.Button(window, text='bottom')
+# b2_1 = tk.Button(window, text='bottom-1')
+# b3 = tk.Button(window, text='left')
+# b3_1 = tk.Button(window, text='left-1')
+# b4 = tk.Button(window, text='right')
+# b4_1 = tk.Button(window, text='right-1')
+# b5 = tk.Button(window, text='center', bg='red')
+#
+# b1.pack(side=tk.BOTTOM)
+# b1_1.pack(side=tk.TOP, fill=tk.X)
+#
+# b2.pack(side=tk.BOTTOM)
+# b2_1.pack(side=tk.BOTTOM, anchor="e")
+#
+# b3.pack(side=tk.LEFT)
+# b3_1.pack(side=tk.LEFT, fill="y")
+#
+# b4.pack(side=tk.RIGHT)
+# b4_1.pack(side=tk.RIGHT, anchor=tk.S)
+#
+# b5.pack(expand=True, fill=tk.BOTH)
+#
+# # side : 특정 위치로 공간 할당. right/left/top/bottom : 부모 컨트롤의 어느쪽 면으로 붙을지, pack 순서대로 가장 바깥쪽으로 배치된다.
+# # anchor : 할당 공간 내 위치 지정. center/n/e/s/w 또는 ne처럼 조합. : 동, 서, 남, 북, 중앙 정렬
+# # expand : 미사용 공간에 확장. True/False
+# # fill : 할당 공간에 대한 크기 맞춤. none/x/y/both : x축, y축 방향으로 컨트롤의 크기를 맞추기
+#
+# # 항상 마지막에
+# window.mainloop()
+
+
+
+# pack 실습
+# import tkinter as tk
+#
+# window = tk.Tk()
+#
+# window.geometry("300x200")
+# window.title("Window Title")
+#
+# b1 = tk.Button(window, text='top')
+# b2 = tk.Button(window, text='top-1')
+# b3 = tk.Button(window, text='top-2')
+# b4 = tk.Button(window, text='top-3')
+#
+#
+# b1.pack(side=tk.TOP, anchor=tk.W)
+# b2.pack(side=tk.TOP, anchor=tk.CENTER)
+# b3.pack(side=tk.TOP, fill=tk.X)
+# b4.pack(side=tk.TOP, anchor=tk.W)
+#
+# # 항상 마지막에
+# window.mainloop()
+
+
+
+# place 절대 배치 방식(좌표계)
+# import tkinter as tk
+#
+# window = tk.Tk()
+#
+# window.geometry("300x300")
+# window.title("Window Title")
+#
+# label = tk.Label(window, text='label1', bg='red')
+# label.place(x=50, y=50)
+#
+# label2 = tk.Label(window, text='label2', bg='green')
+# label2.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+#
+# b1 = tk.Button(window, text='btn')
+# b1.place(relx=0.5, rely=0.8, anchor=tk.CENTER)
+#
+# # 항상 마지막에
+# window.mainloop()
+
+
+
+# 프레임
+# import tkinter as tk
+#
+# window = tk.Tk()
+#
+# window.geometry("1000x1000")
+# window.title("Window Title")
+# window.resizable(width=True, height=1)
+#
+# frame1 = tk.Frame(window, relief='solid', bd=2)
+# frame1.pack(side='left', fill='both', expand=True)
+#
+# frame2 = tk.Frame(window, relief='solid', bd=2)
+# frame2.pack(side='right', fill='both', expand=True)
+#
+# btn1 = tk.Button(frame1, text='f1 버튼')
+# btn1.pack(side='right')
+# btn2 = tk.Button(frame2, text='f2 버튼')
+# btn2.pack(side='left')
+#
+# # 항상 마지막에
+# window.mainloop()
+
+
+
+# 캔버스
+# import tkinter as tk
+#
+# window = tk.Tk()
+#
+# window.geometry("800x600")
+# window.title("Window Title")
+# window.resizable(width=True, height=1)
+#
+# canvas = tk.Canvas(window, width=200, height=200)
+# line = canvas.create_line(10, 10, 20, 20, 20, 130, 30, 140, fill='red')
+# polygon = canvas.create_polygon(50, 50, 170, 170, 100, 170, outline='yellow')
+# arc = canvas.create_arc(100, 100, 300, 300, start=0, extent=150, fill='green')
+#
+# # create_line : 선 긋기 => 좌표쌍, 좌표쌍, ...
+# # create_polygon : 다각형 만들기 => 좌표쌍, 좌표쌍, ...
+# # create_arc : 호 그리기 => 100, 100에서 300, 300 크기를 가지는 start 0 각도에서 시작해서 extent 까지의 각을 갖는 호를 그린다.
+#
+# # 캔버스는 그림, 사진을 올리고 싶을 때 활용.
+#
+# canvas.pack()
+#
+# # 항상 마지막에
+# window.mainloop()
+
+
+# 프로그래스 바
+# import tkinter as tk
+# # 부수적인 컨트롤의 모음
+# import tkinter.ttk
+#
+# window = tk.Tk()
+#
+# window.geometry("800x600")
+# window.title("Window Title")
+#
+# progressbar = tk.ttk.Progressbar(window, maximum=80, mode='indeterminate')
+# progressbar.pack(side=tk.BOTTOM)
+# progressbar.start(50)
+#
+# # start() : ms 단위로 시작 (프로그래스바가 밀리초마다 움직이는 효과)
+# # step(value) : 값 증가, 현재 표시되는 값에서 value만큼 값 추가.
+# # stop() : 종료, 프로그래스바 애니메이션 종료
+#
+# # mode 설정 : determinate, 프로그래스바 표시 스타일. determinate/indeterminate
+# # length : 프로그래스바의 절대적 너비 설정, 기본값은 100이고 상수로 설정 가능
+# # orient : 프로그래스바 표시 방향 vertical/horizontal
+# # maximum : 최대값 설정. 기본값 100
+#
+#
+# # 항상 마지막에
+# window.mainloop()
+
+
+# 프로그래스바2
+# import tkinter as tk
+# import tkinter.ttk
+# import time
+#
+# window = tk.Tk()
+#
+# window.geometry("800x600")
+# window.title("Window Title")
+#
+# progressbar = tk.ttk.Progressbar(window, maximum=100, mode='determinate')
+# progressbar.pack(pady=50)
+#
+# def update_progressbar():
+#     for i in range(100):
+#         progressbar['value'] = i
+#         window.update_idletasks()
+#         time.sleep(0.01)
+# update_progressbar()
+#
+# # 항상 마지막에
+# window.mainloop()
+
+
+
